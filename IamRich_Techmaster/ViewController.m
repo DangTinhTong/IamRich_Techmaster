391445bc262128f0606bc24de28d2d52eaee960e
admin <dangtinhtong@gmail.com>
2017-11-06 15:50:52 +0700

    First Commit

--- IamRich_Techmaster/ViewController.m
+++ IamRich_Techmaster/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *Ruby;
+@property (weak, nonatomic) IBOutlet UILabel *IamRich;
 
 @end
 
@@ -16,8 +18,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+    self.Ruby.alpha = 0;
+    self.IamRich.alpha = 0;
 }
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+   // NSLog(@"ruby's alpha = %1.0f", self.Ruby.alpha);
+    [UIView animateWithDuration:4 animations:^{
+        self.Ruby.alpha = 1;
+       // NSLog(@"ruby's alpha = %1.0f", self.Ruby.alpha);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:3 animations:^{
+            self.IamRich.center = CGPointMake(self.IamRich.center.x, 360);
+            self.IamRich.alpha = 1;
+            
+        } completion:nil];
+    }];
+}
+
+ // Do any additional setup after loading the view, typically from a nib.
+
 
 
 - (void)didReceiveMemoryWarning {
